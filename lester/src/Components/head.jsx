393533ac1f7f0b0af484229331eecc2ad0d5d21c
Suryavanshamikgpian <suryavanasham@kgpian.iitkgp.ac.in>
2025-01-28 19:11:21 +0530

    routing signup and login

--- lester/src/Components/head.jsx
+++ lester/src/Components/head.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import './AllCSS/head.css';
+import Login from './Pages/login.jsx';
+import SignUp from './Pages/SignUp.jsx';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router= createBrowserRouter(
+    [
+        {
+            path: '/login',
+            element : <Login/>
+        },
+        {
+            path: '/signup',
+            element : <SignUp/>
+        }
+
+    ]
+);
 
 const Head = () => {
   return (
     <div>
+        <RouterProvider router={router}/>
       <nav id="nelson">
         <div className="nelson_1stchild">
           <div className="logo">
@@ -20,10 +39,18 @@ const Head = () => {
             <div className="link">Connect Bank</div>
             <div className="link">Transactions</div>
           </div>
-
-          <div className="buttons">
-            <button className="btn">Log in</button>
-            <button className="btn">Sign up</button>
+          <div className="buttons" >
+            <a href='/login'>
+            <button className="btn"  >
+                    <p>Log in</p>
+                </button>
+            </a> 
+                
+            <a href='/signup'>
+            <button className="btn"  >
+                    <p>Sign Up</p>
+                </button>
+            </a> 
           </div>
         </div>
       </nav>
